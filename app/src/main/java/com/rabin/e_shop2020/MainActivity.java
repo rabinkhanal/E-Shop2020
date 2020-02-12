b41238b6905880a3ac36eb9d56d7dd874018a371
DeLL <rabinkhanal2055@gmail.com>
2020-02-12 12:19:05 +0545

    Initial commit

---
+++ app/src/main/java/com/rabin/e_shop2020/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rabin.e_shop2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
